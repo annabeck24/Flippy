63c8d130d5bbe2d1b5a07a4a01302bc1383e69f0
annabeck24 <apbeck24@gmail.com>
2024-06-20 22:03:02 -0400

    Initial Commit

---
+++ Flippy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flippy
+//
+//  Created by Anna on 6/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flippy/FlippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlippyApp.swift
+//  Flippy
+//
+//  Created by Anna on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FlippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
